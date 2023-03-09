fbff0539b2831132f5d6169ea64fe7601c933b66
Carolina-Pinheiro <57190987+Carolina-Pinheiro@users.noreply.github.com>
2023-03-09 19:00:12 +0000

    create token contract (empty)

---
+++ src/Token.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+contract Token{
+
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    receive() external payable {}
+
+    fallback() external {}
+}
