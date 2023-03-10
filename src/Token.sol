a85444ad82c96a3bd5a9849a320de09f4647a1d7
Carolina-Pinheiro <57190987+Carolina-Pinheiro@users.noreply.github.com>
2023-03-10 12:04:41 +0000

    token skeleton contract

--- src/Token.sol
+++ src/Token.sol
@@ -1,15 +1,15 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-contract Token{
+import { OFT } from "@layerZeroOmnichain/token/oft/OFT.sol";
 
-    address public owner;
+/// @title A LayerZero OmnichainFungibleToken example of BasedOFT
+/// @notice Use this contract only on the BASE CHAIN. It locks tokens on source, on outgoing send(), and unlocks tokens when receiving from other chains.
+contract Token is OFT {
+    uint256 public MAX_SUPPLY = 10 ^ 10;
 
-    constructor() {
-        owner = msg.sender;
-    }
+    address private _owner;
+    address public vaultContract;
 
-    receive() external payable {}
-
-    fallback() external {}
+    constructor(address _layerZeroEndpoint) OFT("Token", "TOK", _layerZeroEndpoint) { }
 }
--- test/Token.t.sol
+++ test/Token.t.sol
@@ -3,11 +3,29 @@ pragma solidity ^0.8.0;
 
 import { Test } from "@forge-std/Test.sol";
 import { Token } from "src/Token.sol";
+import { Vault } from "src/Vault.sol";
+import { LZEndpointMock } from "@layerZeroOmnichain/mocks/LZEndpointMock.sol";
 
 contract TokenTest is Test {
     Token token;
+    Vault vault;
+    LZEndpointMock lzEndpoint;
+    address public ownerVault = address(9_032_023);
+    address public barbie = address(2005);
 
-    function setUp() external{
+    function setUp() external {
+        // Set-up the vault contract
+        vm.label(ownerVault, "OwnerVault");
+        vm.label(barbie, "Barbie");
+        vm.startPrank(ownerVault);
+        vault = new Vault();
+        lzEndpoint = new LZEndpointMock(1); // 1 = chainId
+        token = Token(address(lzEndpoint));
+        vm.stopPrank();
     }
 
+    // Tests if the owner is being correctly initialized and saved
+    function testTest() external {
+        // do nothing
+    }
 }
