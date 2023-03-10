dd7340fe12ac297a3d3814119d1adf1ac529386d
Carolina-Pinheiro <57190987+Carolina-Pinheiro@users.noreply.github.com>
2023-03-10 12:33:20 +0000

    fix: update lockUpPeriod logic
    -Updated lockUpPeriod logic to a private mapping
    -change error message to an error "event"

--- src/Vault.sol
+++ src/Vault.sol
@@ -1,19 +1,23 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-contract Vault{
+contract Vault {
+    //-----------------------------------------------------------------------
+    //---------------------------ERRORS-& EVENTS-----------------------------
+    //-----------------------------------------------------------------------
+    error wrongLockUpPeriod();
 
-    
     //-----------------------------------------------------------------------
     //------------------------------VARIABLES--------------------------------
     //-----------------------------------------------------------------------
 
     address public owner;
     uint256 depositID = 0;
+    mapping(uint256 => uint256) private lockUpPeriod; // lockUpPeriod -> rewardsMultiplier
 
     //
-    struct Deposit{
-        uint256 balance;  // number of LP tokens deposit
+    struct Deposit {
+        uint256 balance; // number of LP tokens deposit
         uint256 share; // share of pool given the rewards multiplier
         uint256 depositShareId; // id of the deposit share at the time of the deposit - it will not change position from the linked list since all future nd are added after and le also after - it will be from this id that the reward calculations start
         uint256 id; // id of the deposit
@@ -21,12 +25,12 @@ contract Vault{
     }
 
     mapping(address => Deposit[]) public depositList; // depositsList[owner] = [Deposit deposit1, Deposit, deposit2, ... ]
-    mapping(address => uint256) public rewardsAcrued; // updated when a user tries to claim rewards 
+    mapping(address => uint256) public rewardsAcrued; // updated when a user tries to claim rewards
 
     // There are two situations when a depositShare updates - new deposit (nd) or lock up ends (le)
     // The linked list refers to the depositSharesUpdates, it is organized from past to future (past refers to the beggining of the list, future to the end)
     // TODO: implement linked list
-    struct depositSharesUpdates{
+    struct depositSharesUpdates {
         uint256 nextId;
         uint256 endTime; // in case of nd it is 'now', le it is 'now+lock-up time'
         uint256 shareToReduce; // + for nd, - for le
@@ -34,13 +38,17 @@ contract Vault{
 
     constructor() {
         owner = msg.sender;
+        // Set lock up period
+        lockUpPeriod[6] = 1;
+        lockUpPeriod[1] = 2;
+        lockUpPeriod[2] = 4;
+        lockUpPeriod[4] = 8;
     }
 
-    receive() external payable {}
+    receive() external payable { }
 
-    fallback() external {}
+    fallback() external { }
 
-    
     //-----------------------------------------------------------------------
     //------------------------------EXTERNAL---------------------------------
     //-----------------------------------------------------------------------
@@ -48,8 +56,8 @@ contract Vault{
     /// @notice Function where the user deposits the liquidity, chooses the lock-up period and receives LP tokens
     /// @dev still in development
     /// @param amount: amount of tokens to deposit
-    /// @param lockUpPeriod: lock up period chosen by the user that will determine the rewards multiplier - 0.5 = 6 months, 1 = 1 year, 2 = 2 years, 4 = 4 years
-    function deposit(uint256 amount, uint256 lockUpPeriod) external{
+    /// @param userLockUpPeriod: lock up period chosen by the user that will determine the rewards multiplier - 0.5 = 6 months, 1 = 1 year, 2 = 2 years, 4 = 4 years
+    function deposit(uint256 amount, uint256 userLockUpPeriod) external {
         // Check if any deposit has expired
         checkForDtUpdates();
 
@@ -57,30 +65,28 @@ contract Vault{
 
         // Create a new deposit
         Deposit memory newDeposit = Deposit({
-                                    balance: amount,
-                                    share: amount*getRewardsMultiplier(lockUpPeriod),
-                                    depositShareId: getCurrentShareId(),
-                                    id: depositID,
-                                    owner: msg.sender
-                                    });
-        depositID ++;
+            balance: amount,
+            share: amount * getRewardsMultiplier(userLockUpPeriod),
+            depositShareId: getCurrentShareId(),
+            id: depositID,
+            owner: msg.sender
+        });
+        depositID++;
         depositList[msg.sender].push(newDeposit);
     }
-
 
     /// @notice  Function where the user withdraws the deposits
     /// @dev still in development
     /// @param depositsToWithdraw: list of deposits ids that the user wants to withdraw, if left empty all deposits will be withdrawn
-    function withdraw(uint256[] calldata depositsToWithdraw) external{
+    function withdraw(uint256[] calldata depositsToWithdraw) external {
         // Check if any deposit has expired
         checkForDtUpdates();
     }
 
-
     /// @notice Function where the user can claim the rewards it has accrued
     /// @dev still in development
     /// @param rewardsToClaim: amount of rewards that the user wants to claim, if left empty all rewards will be claimed
-    function claimRewards(uint256 rewardsToClaim) external{
+    function claimRewards(uint256 rewardsToClaim) external {
         // Check if any deposit has expired
         checkForDtUpdates();
     }
@@ -89,44 +95,31 @@ contract Vault{
     //------------------------------INTERNAL---------------------------------
     //-----------------------------------------------------------------------
 
-    /// @notice Function that checks if any deposits have expired and if the total shares needs to be updated 
+    /// @notice Function that checks if any deposits have expired and if the total shares needs to be updated
     /// @dev still in development
-    function checkForDtUpdates() internal{
+    function checkForDtUpdates() internal {
         // TODO
-
     }
-
 
     /// @notice Gets the rewards multiplier according to the lockUpPeriod
     /// @dev still in development
-    /// @param lockUpPeriod: lock up period chosen by the user that will determine the rewards multiplier - 6 = 6 months, 1 = 1 year, 2 = 2 years, 4 = 4 years
+    /// @param userLockUpPeriod: lock up period chosen by the user that will determine the rewards multiplier - 6 = 6 months, 1 = 1 year, 2 = 2 years, 4 = 4 years
     /// @return rewardsMultiplier: the rewards multiplier according to the locking period
-    function getRewardsMultiplier(uint256 lockUpPeriod) internal pure returns(uint256){
-
+    function getRewardsMultiplier(uint256 userLockUpPeriod) internal view returns (uint256) {
         uint256 rewardsMultiplier;
-        if (lockUpPeriod == 6) {
-            rewardsMultiplier = 1;
-        }
-        else if (lockUpPeriod == 1 ){
-            rewardsMultiplier = 2;
-        }
-        else if (lockUpPeriod == 2){
-            rewardsMultiplier = 4;
-        }
-        else if (lockUpPeriod == 4){
-            rewardsMultiplier = 8;
-        }
-        else{
-            revert("Lock up period chosen is not possible, choose between 6 (6 months), 1 (1 year), 2 (2 yeats) or 4 (4 years)");
-        }
 
+        if (lockUpPeriod[userLockUpPeriod] != 0) {
+            rewardsMultiplier = lockUpPeriod[userLockUpPeriod];
+        } else {
+            revert wrongLockUpPeriod();
+        }
         return rewardsMultiplier;
     }
 
     /// @notice Gets the current share id
     /// @dev still in development
     /// @return currentShareId: the current share id
-    function getCurrentShareId() internal pure returns(uint256){
+    function getCurrentShareId() internal pure returns (uint256) {
         // TODO
 
         return 0;
