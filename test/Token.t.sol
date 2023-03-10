d48305aeb990939487c24a33e31708e17c2ef227
Carolina-Pinheiro <57190987+Carolina-Pinheiro@users.noreply.github.com>
2023-03-10 15:32:19 +0000

    build: token contract set-up
    -Token contract is OFT and has access control
    -only vault can mint implemented with openzeppelin accesscontrol
    -tests done for mint feature
    
    Update Token.t.sol
    
    add more actors to the token tests

--- test/Token.t.sol
+++ test/Token.t.sol
@@ -10,22 +10,38 @@ contract TokenTest is Test {
     Token token;
     Vault vault;
     LZEndpointMock lzEndpoint;
-    address public ownerVault = address(9_032_023);
-    address public barbie = address(2005);
+    address public ownerVault = vm.addr(11);
+    address public lucy = vm.addr(12);
+    address public phoebe = vm.addr(13);
+    address public julien = vm.addr(14);
 
     function setUp() external {
         // Set-up the vault contract
         vm.label(ownerVault, "OwnerVault");
-        vm.label(barbie, "Barbie");
+        vm.label(lucy, "Lucy");
+        vm.label(phoebe, "Phoebe");
+        vm.label(julien, "Julien");
         vm.startPrank(ownerVault);
         vault = new Vault();
         lzEndpoint = new LZEndpointMock(1); // 1 = chainId
-        token = Token(address(lzEndpoint));
+        token = new Token(address(lzEndpoint), address(vault));
         vm.stopPrank();
     }
 
     // Tests if the owner is being correctly initialized and saved
-    function testTest() external {
-        // do nothing
+    function testMintOnlyByVault() external {
+        // Vault tries to mint tokens to a user
+        vm.prank(address(vault));
+        token.mint(lucy, 10);
+
+        // Assert that the user received the tokens and the total supply is updated
+        assertEq(token.balanceOf(lucy), 10);
+        assertEq(token.totalSupply(), 10);
+
+        // Someone who is not the vault tries to mint tokens
+        vm.startPrank(phoebe);
+        vm.expectRevert();
+        token.mint(julien, 10);
+        vm.stopPrank();
     }
 }
