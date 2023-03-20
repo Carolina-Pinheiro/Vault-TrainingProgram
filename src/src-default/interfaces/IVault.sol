e3ab4e7091a9570dc0f9253108f73615e488751b
Carolina-Pinheiro <57190987+Carolina-Pinheiro@users.noreply.github.com>
2023-03-20 14:49:31 +0000

    interface update

--- src/src-default/interfaces/IVault.sol
+++ src/src-default/interfaces/IVault.sol
@@ -4,9 +4,22 @@ pragma solidity ^0.8.13;
 import { ILinkedList } from "src/src-default/interfaces/ILinkedList.sol";
 
 interface IVault is ILinkedList {
+    //-----------------------------------------------------------------------
+    //------------------------------LOGS-------------------------------------
+    //-----------------------------------------------------------------------
     event LogUint(uint256);
     event LogRewardsTokenMinted(address, uint256);
     event LogNode(Node);
-    //event LogWithdrawHasNotExpired(uint256);
     event LogDepositExpired(address,uint256); // Owner and deposit id
+    event LogWithdrawHasNotExpired(uint256);
+    event LogUintArray(uint256[]);
+    event LogWithdraw(address,uint256);
+
+    //-----------------------------------------------------------------------
+    //------------------------------ERRORS-----------------------------------
+    //-----------------------------------------------------------------------
+    error NoRewardsToClaimError();
+    error TransferOfLPTokensWasNotPossibleError();
+    error NotEnoughAmountOfTokensDepositedError();
+
 }
