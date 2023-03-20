e30702040ca16f34145fa9ddfd5656b7c334e979
Carolina-Pinheiro <57190987+Carolina-Pinheiro@users.noreply.github.com>
2023-03-20 15:53:09 +0000

    organize and implement interface in vault

--- src/src-default/interfaces/IVault.sol
+++ src/src-default/interfaces/IVault.sol
@@ -10,10 +10,10 @@ interface IVault is ILinkedList {
     event LogUint(uint256);
     event LogRewardsTokenMinted(address, uint256);
     event LogNode(Node);
-    event LogDepositExpired(address,uint256); // Owner and deposit id
+    event LogDepositExpired(address, uint256); // Owner and deposit id
     event LogWithdrawHasNotExpired(uint256);
     event LogUintArray(uint256[]);
-    event LogWithdraw(address,uint256);
+    event LogWithdraw(address, uint256);
 
     //-----------------------------------------------------------------------
     //------------------------------ERRORS-----------------------------------
@@ -21,5 +21,31 @@ interface IVault is ILinkedList {
     error NoRewardsToClaimError();
     error TransferOfLPTokensWasNotPossibleError();
     error NotEnoughAmountOfTokensDepositedError();
+    error WrongLockUpPeriodError();
+    error MsgSenderIsNotVaultError();
 
+    //-----------------------------------------------------------------------
+    //--------------------------FUNCTION-HEADERS-----------------------------
+    //-----------------------------------------------------------------------
+
+    //------------------------------EXTERNAL---------------------------------
+
+    /// @notice Function where the user deposits the Uniswap LP tokens, chooses the lock-up period and receives LP tokens
+    /// @param amount_: amount of Uniswap LP tokens to deposit
+    /// @param lockUpPeriod_: lock up period chosen by the user that will determine the rewards multiplier - 6 = 6 months, 1 = 1 year, 2 = 2 years, 4 = 4 years
+    function deposit(uint256 amount_, uint256 lockUpPeriod_) external;
+
+    /// @notice  Function where the user withdraws the deposits. The list of deposits to withdraw may be chosen.
+    /// @param depositsToWithdraw: list of deposits ids that the user wants to withdraw, if left empty all deposits will try to be withdrawn.
+    function withdraw(uint256[] memory depositsToWithdraw) external;
+
+    /// @notice Function where the user can claim the rewards it has accrued
+    /// @param rewardsToClaim: amount of rewards that the user wants to claim, if left at zero all rewards will be claimed
+    /// @return rewardsToClaim: claimed rewards
+    function claimRewards(uint256 rewardsToClaim) external returns (uint256);
+
+    /// @notice getter to get info about a specific deposit - used in tests
+    /// @param id_: id of the deposit
+    /// @return Node: node that represents the deposit
+    function getDeposit(uint256 id_) external view returns (Node memory);
 }
